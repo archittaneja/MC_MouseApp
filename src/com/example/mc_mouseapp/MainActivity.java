0be4eda7c4c166300c57915903c8641e3f1f9ddf
archittaneja <archit12024@iiitd.ac.in>
2014-12-01 18:25:31 +0530

    Data sent in parallel to Bluetooth

--- src/com/example/mc_mouseapp/MainActivity.java
+++ src/com/example/mc_mouseapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.mc_mouseapp;
-
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
 
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +29,11 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 	private SensorManager sensorManager;	
 	private Sensor gyroscope;
 	TextView xValueText,yValueText,zValueText;
+	Boolean BLUETOOTH_ENABLED = false;
+	BluetoothAdapter adapter; // The default adapter.
+	TextView view = null; // For errors.
+	final int REQUEST_ENABLE_BT_SUCCESS = 1; // Successful Bluetooth enable.
+	private final UUID mouseDroidUUID = UUID.fromString("3DD7E793-C461-4FAE-B715-12E8940A0975");
 	float[] values = new float[3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,57 +42,57 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); 
         sensorManager.registerListener(this, gyroscope ,SensorManager.SENSOR_DELAY_FASTEST);
+        //adapter = BluetoothAdapter.getDefaultAdapter();
+        view = (TextView)findViewById(R.id.textForErrors);
+//        if (!adapter.isEnabled()) {
+//			view.setText("Bluetooth is not enabled. Enabling ...");
+//			Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//			startActivityForResult(enableBluetooth, REQUEST_ENABLE_BT_SUCCESS);
+//		}
+		
+		// Get the Paired Devices.
+        
+		
+		
+		// Select the correct paired device. 
+		// TODO :: Provide user with a list of devices and choose to connect to one.
+		//
+        
     }
+    
+    /**
+	 * Inner class to connect to a device off of the main UI thread.
+	 * @author sukrit
+	 */
+	
 
-    @Override  
+	    @Override  
     protected void onResume()  
     {  
         super.onResume();  
-        
-        
     }  
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        
+    public boolean onOptionsItemSelected(MenuItem item) 
+    {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-
-    
-    
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		new GyroData().execute(event);	
+		new GyroData().executeOnExecutor(GyroData.THREAD_POOL_EXECUTOR,event);	
 		
-//		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-//    	{
-//        	  		
-//        		
-//        			values[0]=event.values[2];
-//        		
-//        			values[1]=event.values[1];
-//        		
-//        			values[2]=event.values[0];
-//        
-//        			xValueText = (TextView) findViewById(R.id.xVal);
-//        	        yValueText =(TextView) findViewById(R.id.yVal);
-//        	        zValueText =(TextView) findViewById(R.id.zVal);
-//        	        xValueText.setText(Float.toString(values[0]));
-//        	        yValueText.setText(Float.toString(values[1]));
-//        	        zValueText.setText(Float.toString(values[2]));
-//        	    
-//        	
-//    	}
+		
 	}
 	
 	class GyroData extends AsyncTask<SensorEvent, Void, float[]>{
@@ -86,53 +100,104 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 	    protected void onPreExecute() {
 	        super.onPreExecute();       
 	    }
-	    
-		
-		protected float[] doInBackground(SensorEvent... event) {
+	    protected float[] doInBackground(SensorEvent... event) {
 			if (event[0].sensor.getType() == Sensor.TYPE_ACCELEROMETER)
 	    	{
-	        		
 	        		String s;
 	        		float temp;
 	        		s= String.format("%.1f", event[0].values[2]);
-	        		
 	        		temp= Float.parseFloat(s);
 	        		if(Math.abs(temp - values[2]) > 0.5)
 	        		{
 	        			values[2] = temp;
 	        		}
-	        		
 	        		s= String.format("%.1f", event[0].values[1]);
-	        		
 	        		temp= Float.parseFloat(s);
-	        		
 	        		if(Math.abs(temp - values[1]) > 0.5)
 	        		{
 	        			values[1] = temp;
 	        		}
 	        		s= String.format("%.1f", event[0].values[1]);
-	        		
 	        		temp= Float.parseFloat(s);
-	        		
-	        		
 	        		if(Math.abs(temp - values[0]) > 0.5)
 	        		{
 	        			values[0] = temp;
 	        		}
-	        		
+	        
 	    	}
 			return values;
 			}
-		protected void onPostExecute(float[] result) {        
-//			xValueText = (TextView) findViewById(R.id.xVal);
-//	        yValueText =(TextView) findViewById(R.id.yVal);
-//	        zValueText =(TextView) findViewById(R.id.zVal);
-//	        xValueText.setText(Float.toString(result[0]));
-//	        yValueText.setText(Float.toString(result[1]));
-//	        zValueText.setText(Float.toString(result[2]));
+	    protected void onPostExecute(float[] result) {        
 			MainActivity.this.setGyroResult(result);
+			
 	    }
 	};
+	
+	
+	
+	
+	class Bluetooth extends AsyncTask<Void, Void, Void>{
+	    @Override
+	    protected void onPreExecute() {
+	        super.onPreExecute();       
+	    }
+	   @SuppressLint("NewApi") @Override
+		protected Void doInBackground(Void... params) {
+		   System.out.println("BLUETOOTH PLS");
+			adapter = BluetoothAdapter.getDefaultAdapter();
+	    	Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+			while (pairedDevices.size() == 0) {
+				pairedDevices = adapter.getBondedDevices();
+			}
+			OutputStream outputStream = null;
+			BluetoothDevice pairedDevice = null;
+			if (pairedDevices.size() > 0) {
+				pairedDevice = (BluetoothDevice) pairedDevices.toArray()[0];
+				ParcelUuid[] uuids = pairedDevice.getUuids();
+                BluetoothSocket socket = null;
+				try {
+					final UUID mouseDroidUUID = UUID.fromString("3DD7E793-C461-4FAE-B715-12E8940A0975");
+					socket = pairedDevice.createRfcommSocketToServiceRecord(mouseDroidUUID);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                try {
+					socket.connect();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                try {
+					outputStream = socket.getOutputStream();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			try {
+				System.out.println("LOHCHUBH PLS");
+				outputStream.write("WTF".getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+	};
+	
+	class SendBluetoothData extends AsyncTask<Void, Void, Void>{
+	    @Override
+	    protected void onPreExecute() {
+	        super.onPreExecute();       
+	    }
+	   @SuppressLint("NewApi") @Override
+		protected Void doInBackground(Void... params) {
+		   		
+			return null;
+		}
+	};
+	
 	
 	public void setGyroResult(float[] result)
 	{
@@ -144,7 +209,12 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         xValueText.setText(Float.toString(result[0]) );
         yValueText.setText(Float.toString(result[1]));
         zValueText.setText(Float.toString(result[2]));
-    	
+        if(!BLUETOOTH_ENABLED)
+		{	System.out.println("BLUETOOTH");
+			BLUETOOTH_ENABLED = true;
+			new Bluetooth().executeOnExecutor(Bluetooth.THREAD_POOL_EXECUTOR);
+		
+		}
 	}
 	
 	@Override
